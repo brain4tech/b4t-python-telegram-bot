664f6071554464b304d3b61c768ef56d8f7bff9c
Jefta de Leeuw van Weenen <Jefta.deLeeuw@web.de>
2021-09-17 18:15:48 +0200

    edited template for correct importing

--- tests/template.py
+++ tests/template.py
@@ -1,9 +1,19 @@
-from . import telegram_bot as tb
+
+# --- QUICK AND DIRTY ---
+import sys, os
+
+dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(dir)
+os.chdir(parent_dir)
+sys.path.append(parent_dir)
+# -----------------------
+
+from telegram_bot import TelegramBot
 
 # insert own bot-token here
 token = ""
 
-bot = tb.TelegramBot(bot_token_= token)
+bot = TelegramBot(bot_token_= token)
 
 
 while True:

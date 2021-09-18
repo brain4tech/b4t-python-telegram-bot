d4c59c7568a1e577eda209c5996bd66dd2cbd15a
Jefta de Leeuw van Weenen <Jefta.deLeeuw@web.de>
2021-09-18 21:23:14 +0200

    toBotDict(): parse buttons to usable dict directly

--- classes/keyboards.py
+++ classes/keyboards.py
@@ -26,36 +26,55 @@ class KeyboardButton:
 class ButtonList:
     def __init__(self, button_type_: type, button_list_: list = []):
 
-        self.button_type = None
-        self.button_type_str = ""
+        self.__button_type = None
+        self.__button_type_str = ""
         self.__button_list = []
 
         if button_type_ == InlineButton:
-            self.button_type = button_type_
-            self.button_type_str = "inline"
+            self.__button_type = button_type_
+            self.__button_type_str = "inline"
         elif button_type_ == KeyboardButton:
-            self.button_type = button_type_
-            self.button_type_str = "keyboard"
+            self.__button_type = button_type_
+            self.__button_type_str = "keyboard"
         else:
             raise TypeError("given button_type is not type InlineButton or KeyboardButton")
 
         if button_list_:
             for element in button_list_:
-                if isinstance(element, self.button_type):
-                    self.button_list.append(element)
-
+                if isinstance(element, self.__button_type):
+                    self.__button_list.append(element)
 
     def __str__(self):
         return str(self.toDict())
 
     def toDict(self):
-        return [button.toDict() for button in self.list]
+        return [button.toDict() for button in self.__button_list]
 
     def addCommand(self, button_):
-        if isinstance(button_, self.button_type):
-            self.button_list.append(button_)
+        if isinstance(button_, self.__button_type):
+            self.__button_list.append(button_)
     
     def bulkAddCommands (self, button_list: list):
         for element in button_list:
-            if isinstance(element, self.button_type):
-                self.button_list.append(element)
+            if isinstance(element, self.__button_type):
+                self.__button_list.append(element)
+    
+    def getButtonType(self):
+        return self.__button_type
+
+    def toBotDict(self, special_button: type = None, column_count: int = 3):
+
+        button_list = []
+        button_row = []
+
+        for button in self.__button_list:
+            button_row.append(button.toDict())
+
+            if len(button_row) >= column_count or button == self.__button_list[-1]:
+                button_list.append(button_row[:])
+                button_row.clear()
+
+        if special_button and isinstance(special_button, self.__button_type):
+            button_list.append([special_button.toDict()])
+
+        return {f'{"inline_" if self.__button_type_str == "inline" else ""}keyboard': button_list}

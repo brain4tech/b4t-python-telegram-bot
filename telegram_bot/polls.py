0779031b383df114cd761fcf4ef834f665b7adc4
Jefta de Leeuw van Weenen <Jefta.deLeeuw@web.de>
2021-09-20 23:17:23 +0200

    moved classes to parent dir

--- telegram_bot/polls.py
+++ telegram_bot/polls.py
@@ -1,6 +1,7 @@
 # classes for polls
 
-from classes.messages import User
+from .messages import User
+
 
 class Poll:
     def __init__(self, data_: dict):
@@ -12,15 +13,21 @@ class Poll:
         self.is_anonymous = data_['is_anonymous']
         self.type = data_['type']
         self.allows_multiple_answers = data_['allows_multiple_answers']
-        self.correct_option_id = data_['correct_option_id'] if 'correct_option_id' in data_ else None
-        self.explanation = data_['explanation'] if 'explanation' in data_ else None
-        self.open_period = data_['open_period'] if 'open_period' in data_ else None
-        self.closed_data = data_['closed_data'] if 'closed_data' in data_ else None
+        self.correct_option_id = data_[
+            'correct_option_id'] if 'correct_option_id' in data_ else None
+        self.explanation = data_[
+            'explanation'] if 'explanation' in data_ else None
+        self.open_period = data_[
+            'open_period'] if 'open_period' in data_ else None
+        self.closed_data = data_[
+            'closed_data'] if 'closed_data' in data_ else None
+
 
 class PollOption:
     def __init__(self, data_: dict):
         self.text = data_['text']
         self.voter_count = data_['voter_count']
+
 
 class PollAnswer:
     def __init__(self, data_: dict):

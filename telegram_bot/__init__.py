e8d9efd7159d2f4512e640042c827c2ce0fd23af
Jefta de Leeuw van Weenen <Jefta.deLeeuw@web.de>
2021-09-20 23:18:57 +0200

    added init

---
+++ telegram_bot/__init__.py
@@ -0,0 +1,14 @@
+# __init__.py
+
+# version of package
+__version__ = "1.0.0"
+
+# importing all classes
+from .bot import TelegramBot
+from .buttons import KeyboardButton, InlineButton, ButtonList
+from .commands import BotCommand, BotCommandList
+from .media import Photo, Video, Audio, Document
+from .messages import Message, ChannelPost, MessageEntity, MessageEntityList, User, Chat
+from .misc import CallbackQuery
+from .polls import Poll, PollOption, PollAnswer
+from .updates import Update

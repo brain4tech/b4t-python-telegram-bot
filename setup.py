54cfb2f74d181a8d3ae9ea81c36ffc9ed37a9bd2
Brain4Tech <59681472+brain4tech@users.noreply.github.com>
2021-09-21 12:30:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
 from setuptools import setup, find_packages
+import subprocess
+import os
+
+cf_remote_version = (
+    subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+    .stdout.decode("utf-8")
+    .strip()
+)
+assert "." in cf_remote_version
+
+assert os.path.isfile("cf_remote/version.py")
+with open("cf_remote/VERSION", "w", encoding="utf-8") as fh:
+    fh.write(f"{cf_remote_version}\n")
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 classifiers = [
     'Development Status :: 5 - Production/Stable',
@@ -10,11 +26,11 @@ classifiers = [
 
 setup(
     name='b4t-python-telegram-bot',
-    version='1.0.0',
+    version=cf_remote_version,
     description='Simple and lightweight Telegram Bot',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/brain4tech",
+    url="https://github.com/brain4tech\b4t-python-telegram-bot",
     author='Brain4Tech',
     author_email='brain4techyt@gmail.com',
     license='MIT',

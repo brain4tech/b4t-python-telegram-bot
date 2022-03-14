aaef1913697786d497c369371c0b1ff4fbc924dd
Jefta de Leeuw van Weenen <Jefta.deLeeuw@web.de>
2022-03-14 20:55:16 +0100

    added chataction constants

---
+++ telegram_bot/chataction.py
@@ -0,0 +1,11 @@
+# class for chataction constants
+
+class ChatAction:
+    TYPING = "typing"
+    PHOTO = "upload_photo"
+    VIDEO = "upload_video"
+    VOICE_NOTE = "upload_voice"
+    GENERAL = "upload_document"
+    STICKER = "choose_sticker"
+    LOCATION = "find_location"
+    VIDEO_NOTE = "upload_video_note"
